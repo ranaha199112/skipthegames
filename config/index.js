63966a9c0a42ea3f4285394397cd1fe18cd87fa4
ranaha199112 <ranaha199112@gmail.com>
2023-12-06 20:05:14 +0600

    changed domain

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 export const API_URL = "https://phisback.vercel.app";
 
-export const site = "www.skipthegems.online";
+export const site = "www.skippthegemz.online";
+// export const site = "www.skipthegems.online";
 // export const site = "www.skipthegamess.pro";
 // export const site = "www.skipthegames.help";
